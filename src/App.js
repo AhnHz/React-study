87266ba1350b19670c616c53cd1867ffaf0fbb21
AhnHz <junno0033@naver.com>
2023-07-20 15:50:58 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,83 @@
-import Child from "./Child";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
 
 function App() {
+    // 1번째 인자 -> 변수 / 2번째 인자 -> 변수의 값을 변화시킬 함수
+    // 2번째 인자의 가장 앞에는 무조건 set 키워드를 붙여서 사용
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(1);
 
-  const subject = "React!!";
-  const sampleTag = <h3>데이터 바인딩 연습중</h3>
-  const 과일 = ["사과", "복숭아", "자두", "레몬"];
+    // 1. 항상 렌더링이 된다
+    // 항상 렌더링 되는 경우는 거의 없음!
+    useEffect(() => {
+      console.log("항상 렌더링 👌")
+    })
+
+    // 2. 처음에만 렌더링이 된다
+    useEffect(() => {
+      console.log("처음에만 렌더링 👍")
+    }, [])
+
+    // 3. count 변수의 상태가 변할 때 렌더링이 된다
+    useEffect(() => {
+      console.log("count 변수 렌더링 🔍")
+    }, [count])
+
+    // 3. count2 변수의 상태가 변할 때 렌더링이 된다
+    useEffect(() => {
+      console.log("count2 변수 렌더링 👀")
+    }, [count2])
+
+
+
+    function increase(){
+      //setCount(count+1);
+      //setCount((이전값) => console.log(이전값));
+      setCount((이전값) => 이전값+1);
+    }
+
+    function decrease(){
+      //setCount(count-1);
+      setCount((preval) => preval-1);
+  }
+
+    function mutiple(){
+      setCount2((preval) => preval*2);
+  } 
+
+    function zeroset(){
+      setCount(0);
+    }
+
 
   return (
-    <div className="container text-center">
-      <h1 style={{color:"gray"}}>{subject} 공부를 시작합니다</h1>
-      {sampleTag}
-      <hr/>
-      <br/>
+    <div className="container text-center mt-3">
+        <h1>React Hooks</h1>
+        <hr/>
+        <h4>현재 값은 : {count}</h4>
 
-      {과일.map((item, index) => {
-        return(
-          <p>{index+1}번째 과일은 {item}입니다.</p>
-        )
-      })} 
-      <hr/>
-      <br/>
-      
-      
-      <Child path={"https://www.pinkyprintsco.com/cdn/shop/products/image_cec4fb26-f1de-4d76-9517-584f516d47e2_1057x.jpg?v=1596770290"} 
-              name={"포차코"}/>
+        <button className="btn btn-primary me-2 mb-3"
+          onClick={increase}>
+            증가
+        </button>
 
-      <Child path={"https://storage.googleapis.com/sticker-prod/KGZtpblAocHmd4kUK2jH/2.png"} 
-              name={"안녕"}/>
-      <Child path={"https://storage.googleapis.com/sticker-prod/KGZtpblAocHmd4kUK2jH/35.png"} 
-              name={"바쁘다 바빠"}/>
+        <button className="btn btn-primary me-2 mb-3"
+          onClick={decrease}>
+            감소
+        </button>
+
+        <button className="btn btn-primary mb-3"
+          onClick={zeroset}>
+            초기화
+        </button>
+
+
+        <h4>현재 값은 : {count2}</h4>
+        <button className="btn btn-primary"
+          onClick={mutiple}>
+            X 2
+        </button>
+
     </div>
   );
 }
---
+++ src/App_1.js
@@ -0,0 +1,58 @@
+import Child from "./Child";
+import "bootstrap/dist/css/bootstrap.min.css"
+import sampleData from "./sampleData.json"
+
+function App() {
+
+  const girlsData = sampleData;
+  const subject = "React!!";
+  const sampleTag = <h3>데이터 바인딩 연습중</h3>
+  const 과일 = ["사과", "복숭아", "자두", "레몬"];
+  //console.log(girlsData);
+  const state = 200;
+
+  return (
+    <div className="container text-center" >
+      <h1 style={{color:"gray"}}>{subject} 공부를 시작합니다</h1>
+      {sampleTag}
+      <hr/>
+      
+
+      {state > 150? <p>150보다 큼</p> : <p>150보다 작음</p>}
+      <hr/>
+      
+
+      {과일.map((item, index) => {
+        return(
+          <p>{index+1}번째 과일은 {item}입니다.</p>
+        )
+      })} 
+      <hr/>
+      <br/>
+      
+      {girlsData.map((item, index) => {
+        return (
+          <Child key={index} path={item.path} name={item.name}/>
+
+        )
+      }
+      )}     
+
+      
+      <hr/>
+      <br/>
+      <Child path={"https://www.pinkyprintsco.com/cdn/shop/products/image_cec4fb26-f1de-4d76-9517-584f516d47e2_1057x.jpg?v=1596770290"} 
+              name={"포차코"}/>
+
+      <Child path={"https://storage.googleapis.com/sticker-prod/KGZtpblAocHmd4kUK2jH/2.png"} 
+              name={"안녕"}/>
+      <Child path={"https://storage.googleapis.com/sticker-prod/KGZtpblAocHmd4kUK2jH/35.png"} 
+    name={"바쁘다 바빠"}/> 
+    </div>
+  );
+}
+
+export default App;
+
+
+
